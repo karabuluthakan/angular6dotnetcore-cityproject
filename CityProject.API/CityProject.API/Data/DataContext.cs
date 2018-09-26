77150262d61ef41a1740a02359d5f99d7784be03
karabuluthakan <hakan.karabulut@ugurbilgi.net>
2018-09-26 22:18:34 +0300

    DataContext configuration completed

---
+++ CityProject.API/CityProject.API/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityProject.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityProject.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+    }
+}
